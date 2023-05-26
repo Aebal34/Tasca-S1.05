11fdf844073d34ced9968a21593daf0e748a1dd6
Joren <97386268+Aebal34@users.noreply.github.com>
2023-05-26 12:51:15 +0200

    config.properties loaded into App

--- src/main/java/n2exercici1/App.java
+++ src/main/java/n2exercici1/App.java
@@ -2,15 +2,35 @@ package n2exercici1;
 
 import java.io.*;
 import org.apache.commons.configuration2.*;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.*;
+
+import org.apache.commons.configuration2.ex.ConfigurationException;
+
+
 
 public class App {
 
 	public static void main(String[] args) {
 		
-		PropertiesConfiguration config = new PropertiesConfiguration();
+		FileBasedConfiguration config = null;
+		Parameters params = new Parameters();
+		FileBasedConfigurationBuilder<PropertiesConfiguration> builder = 
+				new FileBasedConfigurationBuilder<PropertiesConfiguration>(PropertiesConfiguration.class)
+				.configure(params.properties()
+						.setFileName("config.properties")
+						.setBasePath(System.getProperty("user.dir")));
 		
-		String path = System.getProperty("user.dir");
-		File textFile = new File(path+"\\directories.txt");
+		try {
+			config = builder.getConfiguration();
+			System.out.println("No ha pillado exception!");
+		} catch (ConfigurationException e) {
+			e.getMessage();
+			System.out.println("Ha pillado exception!");
+		}
+		
+		String path = (String) config.getProperty("directoryPath");
+		File textFile = new File(path+(String)config.getProperty("fileName"));
 		
 		try {
 			ListFilesInDirectory.listDirectoryFilesRecursive(path, 0, textFile);
